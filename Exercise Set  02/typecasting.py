ea19788cabc96ddf223e267b54beee7669277f97
itsianfrane <marianitofrane@gmail.com>
2024-08-28 12:25:59 +0800

    Add files via upload

---
+++ Exercise Set  02/typecasting.py
@@ -0,0 +1,5 @@
+integer_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+difference = float(integer_input) - float_input 
+
+print("Difference:", "{:.2f}".format(difference))
